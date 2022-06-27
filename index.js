dbc2cf42ebdb5c1e112d261e083071ef765c2128
manishchandra-resiliencesoft <manishchandra@resiliencesoft.com>
2022-06-27 16:26:25 +0530

    setup fetch product route

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-const express = require('express');
-const products=require('./assets/products.json');
-const app = express()
+const express = require("express");
+const products = require("./assets/products.json");
+const app = express();
 
-
-app.get('/',(req, res) => {
-  res.send('Hello World')
+app.get("/", (req, res) => {
+	res.send("Hello World");
 });
 
-app.get('/getProducts/',(req, res) => {
-
-  let cat=[];
-    let data=products.filter((curData)=>{
-
-      if(curData.category==="jewelery"){
-          cat.push(curData);
-      }    
-    
-    })
-    res.send(cat);
-  });
+app.get("/products", (req, res) => {
+	res.send(products);
+});
 
 app.listen(3000);
